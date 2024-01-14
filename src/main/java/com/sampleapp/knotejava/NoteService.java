79da864adf2b236fad57f58040e30a8c31a35ed8
qeselan <taniyici.omer@gmail.com>
2024-01-14 14:08:57 -0800

    add rendering markdown to html

--- src/main/java/com/sampleapp/knotejava/NoteService.java
+++ src/main/java/com/sampleapp/knotejava/NoteService.java
@@ -1,5 +1,8 @@
 package com.sampleapp.knotejava;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -13,6 +16,9 @@ public class NoteService {
     @Autowired
     private NotesRepository notesRepository;
 
+    private Parser parser = Parser.builder().build();
+    private HtmlRenderer renderer = HtmlRenderer.builder().build();
+
     public void getAllNotes(Model model) {
         List<Note> notes = notesRepository.findAll();
         Collections.reverse(notes);
@@ -21,7 +27,12 @@ public class NoteService {
 
     public void saveNote(String description, Model model) {
         if (description != null && !description.trim().isEmpty()) {
-            notesRepository.save(new Note(null, description.trim()));
+            // translate markup to html
+            Node document = parser.parse(description.trim());
+            String html = renderer.render(document);
+            notesRepository.save(new Note(null, html));
+
+            // clean up textarea
             model.addAttribute("description", "");
         }
     }
